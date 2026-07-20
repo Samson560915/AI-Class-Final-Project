a67feae5661c9360268770acbcdd4baf19ced9fd
elip2 <elip271828@gmail.com>
2026-07-20 11:31:09 -0400

    Implemented turtle draw function

--- Main.py
+++ Main.py
@@ -1,17 +1,21 @@
 from Node import Node
 from Searches import a_star, local
+from Draw import draw
 import random
 
 def get_points(num):
     s = []
     for i in range(num):
-        x = random.randint(1, 10)
-        y = random.randint(1, 10)
+        x = random.randint(-400, 400)
+        y = random.randint(-400, 400)
         s.append((x, y))
     return s
 
-points = get_points(5)
+points = get_points(25)
 print(points)
 
-for i in local(points):
+nodes = local(points)
+for i in nodes:
     print((i.x,i.y))
+
+draw(nodes)
